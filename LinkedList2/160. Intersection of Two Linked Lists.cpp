82355fcb5dad9afd5b7f197f13a81c7c6801c613
rohit sigar <rohitsigar05@gmail.com>
2022-06-20 23:29:18 +0530

    my soultion of 160. Intersection of Two Linked Lists

---
+++ LinkedList2/160. Intersection of Two Linked Lists.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        
+        
+        int n = 0;
+        int m = 0;
+        
+        ListNode *tempA = headA;
+        ListNode *tempB = headB;
+        
+        while(tempA!=NULL)
+        {
+            n++;
+            tempA = tempA->next;
+        }
+        
+        while(tempB!=NULL)
+        {
+            m++;
+            tempB = tempB->next;
+        }
+        
+        if(m > n)
+        {
+            swap(headA , headB);
+            swap(n ,m);
+        }
+        
+        while(n > m)
+        {
+            headA = headA->next;
+            n--;
+        }
+        
+        while(headA!=NULL)
+        {
+            if(headA==headB)
+                return headA;
+            headA = headA->next;
+            headB = headB->next;
+        }
+        return headA;
+        
+        
+        
+        
+    }
+};
